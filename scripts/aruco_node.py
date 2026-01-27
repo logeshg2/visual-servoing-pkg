57e0678720827e5892017780e16a2c7a91fc0fd0
Logesh <logeshg248@gmail.com>
2026-01-27 10:54:48 +0530

    changes

--- scripts/aruco_node.py
+++ scripts/aruco_node.py
@@ -6,14 +6,18 @@ Aruco is used in this script.
 """
 
 import cv2
+import pickle
 import numpy as np
+from scipy.spatial.transform import Rotation
 
 import rclpy
 from rclpy.node import Node
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
+from tf2_ros import TransformBroadcaster
 from std_msgs.msg import Int64MultiArray
 from visual_servoing_pkg.msg import ArucoCorner
+from geometry_msgs.msg import Pose, TransformStamped
 from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
 
 class ArucoNode(Node):
@@ -34,17 +38,25 @@ class ArucoNode(Node):
         self.cam_obj.set(cv2.CAP_PROP_FRAME_HEIGHT, self.frame_height)
         self.frame_center = np.array([int(self.frame_width // 2), int(self.frame_height // 2)])
 
+        # camera intrinsic's
+        K_fp = open("/home/logesh/fanuc_ws/src/visual-servoing-pkg/config/camera_matrix.pkl", "rb")
+        dist_fp = open("/home/logesh/fanuc_ws/src/visual-servoing-pkg/config/dist_coef.pkl", "rb")
+        self.K = pickle.load(K_fp)
+        self.camDist = pickle.load(dist_fp)
+
         # logging
         self.get_logger().info(f"Camera device id: {self.cam_dev_id}")
         self.get_logger().info(f"Image frame width: {self.frame_width}")
         self.get_logger().info(f"Image frame height: {self.frame_height}")
 
         # aruco detector
-        aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250)
+        aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
+        self.markerLength = 0.1
         parameters = cv2.aruco.DetectorParameters()
         self.detector = cv2.aruco.ArucoDetector(aruco_dict, parameters)
         self.aruco_center = None
         self.corner = None
+        self.aruco_pose = None
 
         # target aruco points
         self.tar_top_left = np.array([370, 149])
@@ -54,10 +66,12 @@ class ArucoNode(Node):
 
         # ros2 communication variables
         self.cvBridge = CvBridge()
+        self.tf_broadcaster = TransformBroadcaster(self)
         self.img_group = MutuallyExclusiveCallbackGroup()
         self.img_publisher = self.create_publisher(Image, "/processed_image", 10, callback_group=self.img_group)
         self.aruco_center_publisher = self.create_publisher(Int64MultiArray, "/aruco_center", 10)
         self.corner_pub = self.create_publisher(ArucoCorner, "/aruco_corners", 10)
+        self.aruco_pose_pub = self.create_publisher(Pose, "/aruco_pose", 10)
 
         # image reader timer
         self.img_reader_timer = self.create_timer(1/20, self.image_reader_timer, callback_group=self.img_group)     # 20 hz
@@ -87,9 +101,27 @@ class ArucoNode(Node):
             cy = p1[1] + (p3[1] - p1[1]) // 2
             self.aruco_center = np.array([int(cx), int(cy)])
             cv2.circle(self.latest_image, self.aruco_center, radius=5, thickness=-1, color=(255, 0, 0))
+
+            # aruco pose
+            object_points = np.array([
+                [-self.markerLength / 2, -self.markerLength / 2, 0],
+                [self.markerLength / 2, -self.markerLength / 2, 0],
+                [self.markerLength / 2, self.markerLength / 2, 0],
+                [-self.markerLength / 2, self.markerLength / 2, 0]
+            ], dtype=np.float32)
+            _, rvec, tvec = cv2.solvePnP(object_points, np.array(self.corner), self.K, self.camDist, False, flags=cv2.SOLVEPNP_EPNP )
+            if (_):
+                cv2.drawFrameAxes(self.latest_image, self.K, self.camDist, rvec, tvec, 0.1, 3)
+                rvec = rvec.flatten()
+                tvec = tvec.flatten()
+                quat = Rotation.from_rotvec(rvec).as_quat()
+                self.aruco_pose = {'tvec': tvec, 'quat': quat}
+            else:
+                self.aruco_pose = None
         else:
             self.aruco_center = np.array([-1, -1])
             self.corner = None
+            self.aruco_pose = None
 
     def plotTargetMarkers(self):
         # mark center of the frame
@@ -118,6 +150,23 @@ class ArucoNode(Node):
         # cv2.line(self.latest_image, self.tar_bottom_right, self.tar_bottom_left, (230, 216, 173), 2)
         # cv2.line(self.latest_image, self.tar_bottom_left, self.tar_top_left, (230, 216, 173), 2)
 
+    def TF_publisher(self, trans, quat):
+        # broadcast cTo tf
+        tf = TransformStamped()
+        tf.header.stamp = self.get_clock().now().to_msg()
+        tf.header.frame_id = 'camera_link'
+        tf.child_frame_id = "aruco_target"
+        
+        tf.transform.translation.x = trans[0]
+        tf.transform.translation.y = trans[1]
+        tf.transform.translation.z = trans[2] - 0.3        # NOTE: publish target at a certain height
+
+        tf.transform.rotation.x = quat[0]
+        tf.transform.rotation.y = quat[1]
+        tf.transform.rotation.z = quat[2]
+        tf.transform.rotation.w = quat[3]
+
+        self.tf_broadcaster.sendTransform(tf)
 
     def image_reader_timer(self):
         # acquire latest image
@@ -135,7 +184,7 @@ class ArucoNode(Node):
         self.process_image()
 
         # draw target markers
-        self.plotTargetMarkers()        
+        # self.plotTargetMarkers()        
 
         # publish aruco center and aruco corners array
         if (self.aruco_center is not None and self.corner is not None):
@@ -151,6 +200,21 @@ class ArucoNode(Node):
             corner_msg.bottom_right = np.array(self.corner[2], np.int64)
             corner_msg.bottom_left = np.array(self.corner[3], np.int64)
             self.corner_pub.publish(corner_msg)
+
+            # aruco pose + aruco TF
+            if (self.aruco_pose is not None):
+                msg = Pose()
+                msg.position.x = self.aruco_pose['tvec'][0]
+                msg.position.y = self.aruco_pose['tvec'][1]
+                msg.position.z = self.aruco_pose['tvec'][2]
+                msg.orientation.x = self.aruco_pose['quat'][0]
+                msg.orientation.y = self.aruco_pose['quat'][1]
+                msg.orientation.z = self.aruco_pose['quat'][2]
+                msg.orientation.w = self.aruco_pose['quat'][3]
+                self.aruco_pose_pub.publish(msg)
+
+                # aruco TF (with respect to camera_link)
+                self.TF_publisher(self.aruco_pose['tvec'], self.aruco_pose['quat'])
         else:
             corner_msg = ArucoCorner()
             corner_msg.top_left = np.array([-1, -1])
